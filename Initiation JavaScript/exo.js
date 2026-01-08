6a23f3f97d8857f68a5e9a35f3dd60d59571ca80
djokotonicolas@gmail.com <djokotonicolas@gmail.com>
2026-01-08 09:58:06 +0100

    test

---
+++ Initiation JavaScript/exo.js
@@ -0,0 +1,6 @@
+// declaration des variables et contantes
+let age = 18;
+const nom = Nicolas;
+let titre = document.querySelector("#titre");
+titre.textContent = "netflixfr";
+titre.style.color = "red";
